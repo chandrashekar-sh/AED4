c28514f96692f1f74f9f1ff47395fa7e31811ab6
Sharan Chandra Shekar <chandrashekar.sh@northeastern.edu>
2021-03-30 12:24:00 -0400

    First commit

---
+++ AED4/Assignment4Skeleton/db4odemo/src/Business/Restaurant/Order.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author css19
+ */
+public class Order {
+     private String orderId;
+    private String restaurantName;
+    private String CustomerName;
+    private String DeliveryMan;
+    private ArrayList<Food> Order;
+    private String price;
+    private String status;
+    private String deliveryAddress;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    public void setCustomerName(String CustomerName) {
+        this.CustomerName = CustomerName;
+    }
+
+    public String getDeliveryMan() {
+        return DeliveryMan;
+    }
+
+    public void setDeliveryMan(String DeliveryMan) {
+        this.DeliveryMan = DeliveryMan;
+    }
+
+    public ArrayList<Food> getOrder() {
+        return Order;
+    }
+
+    public void setOrder(ArrayList<Food> Order) {
+        this.Order = Order;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        return orderId;
+    }
+}
